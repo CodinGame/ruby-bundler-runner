087c70c5a346426573f544345f4217f692be9b48
Julien Poulton <julien.poulton@gmail.com>
2017-02-10 12:28:27 +0100

    copy answer2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /project/target
 
-cp /-r project/answer/* .
+cp -r project/answer/* .
 
 bundle config --local path ./bundler_gems 1>/dev/null 2>/dev/null
 bundle exec bin/rspec --init 1>/dev/null 2>/dev/null
